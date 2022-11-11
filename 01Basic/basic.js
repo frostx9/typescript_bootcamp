4d38739cc2a55b4f8a758e7f21ef3beea6d2c353
Saumya Som <saumyasom92@gmail.com>
2022-11-11 23:51:03 +0530

    Typescript Bootcamp

--- 01Basic/basic.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Typescript !!");
-console.log(Math.round(15.22));
--- 02Data_Types/type.ts
+++ 02Data_Types/type.ts
@@ -1,8 +1,11 @@
-const movie: string = "Top Gun";
-console.log(movie);
+// Type Declaration 
+// it start with ' : '
 
-const catLives: number = 9;
-console.log(catLives);
+const movie: string = "Top Gun" // in here we declare movie variable always will be 'string'
+console.log(movie)
 
-const gameOver: boolean = true;
-console.log(gameOver);
+const catLives: number = 9 // in here we declare movie variable always will be 'number'
+console.log(catLives)
+
+const gameOver: boolean = true // in here we declare movie variable always will be 'boolean'
+console.log(gameOver)
